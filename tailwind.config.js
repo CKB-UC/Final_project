31a8c397e8e1d62052251d2c203ef803e96047bb
Cedrick Keanu Balos <baloskeanu@gmail.com>
2024-04-17 20:27:32 +0800

    adding routes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./resources/**/*.balde.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue"
+  ],
   theme: {
     extend: {},
   },
